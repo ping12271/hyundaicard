6dd0cb286680860bf186440e5266d4707ea09991
Hyeeun Shin <hyeeunshin@Hyeeunui-iMac.local>
2020-06-16 03:20:32 +0900

    컬처라이프완료

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,9 @@ var hyundaicard = {
         hyundaicard.threeSlideshow()
         hyundaicard.singleSlideshow()
         hyundaicard.fadeSlideshow()
+        hyundaicard.navSlideshow()
+        hyundaicard.backTop()
+        hyundaicard.getScroll()
     },
 
     threeSlideshow: function () {
@@ -38,5 +41,66 @@ var hyundaicard = {
             cssEase: 'linear'
         });
     },
+
+    navSlideshow: function () {
+        $('.gallery-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.gallery-nav'
+        });
+        $('.gallery-nav').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            asNavFor: '.gallery-for',
+            centerMode: true,
+            focusOnSelect: true
+        });
+
+    },
+
+
+
+    getScroll: function () {
+        $(window).on('scroll', function () {
+            const scrollTop = $(window).scrollTop();
+            const headerHeight = $('#header').innerHeight();
+            const visualHeight = $('.visual').innerHeight();
+
+            if(scrollTop > 50) {
+                $('html').addClass('show-backtop')
+            } else {
+                $('html').removeClass('show-backtop');
+            }
+
+            if(scrollTop > headerHeight) {
+                $('html').addClass('show-title-b')
+            } else {
+                $('html').removeClass('show-title-b')
+            }
+
+            if(scrollTop > visualHeight) {
+                $('html').addClass('show-title-w')
+            } else {
+                $('html').removeClass('show-title-w');
+            }
+
+            if(scrollTop > 5) {
+                $('html').addClass('show-bar')
+            } else {
+                $('html').removeClass('show-bar');
+            }
+
+        })
+    },
+
+    backTop: function () {
+        $('.back-top').on('click', function () {
+            $('html, body').animate({
+                scrollTop:0
+            },400)
+        })
+    }
 
 };
